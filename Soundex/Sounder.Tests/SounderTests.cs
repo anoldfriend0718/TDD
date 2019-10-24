c2c8ddb81ccd8a2366ccc62447b37e691382ba3a
Qianghui Xu <QXu10@slb.com>
2019-10-24 15:40:32 +0800

    complete second tests

--- Soundex/Sounder.Tests/SounderTests.cs
+++ Soundex/Sounder.Tests/SounderTests.cs
@@ -1,22 +1,35 @@
 using System;
 using Xunit;
-using Soundex;
 
-namespace Sounder.Tests
+namespace TDDPractices.Classes.Tests
 {
     public class SounderTests
     {
         [Theory]
         [InlineData("Apple","Appl")]
-        [InlineData("Inline","Inlin")]
-        [InlineData("Outing","utng")]
-        [InlineData("Year","r")]
-        [InlineData("While","l")]
-        public void Should_Abandon_A_E_I_O_U_Y_H_W(string input, string output)
+        [InlineData("Inline","Inln")]
+        [InlineData("Outing", "Otng")]
+        [InlineData("Year","Yr")]
+        [InlineData("While","Wl")]
+        public void Should_Abandon_A_E_I_O_U_Y_H_W(string input, string expectedOutput)
         {
-            var soundex =new Soundex.Soundex();
-            string actual = soundex.AbandonLettersofAEIOUYHW();
-            Assert.Equal(input,output);
+            var soundex =new Soundex();
+            string actualOutput = soundex.AbandonLettersofAEIOUYHW(input);
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Theory]
+        [InlineData('b', 1)]
+        [InlineData('c', 2)]
+        [InlineData('d', 3)]
+        [InlineData('l', 4)]
+        [InlineData('m', 5)]
+        [InlineData('r', 6)]
+        public void Should_Replace_Cononant_With_Number(char c,int expectedNum)
+        {
+            var soundex = new Soundex();
+            int actualNum=soundex.ReplaceCononantWithNumber(c);
+            Assert.Equal(expectedNum,actualNum);
         }
     }
 }
--- Soundex/Soundex/Soundex.cs
+++ Soundex/Soundex/Soundex.cs
@@ -1,10 +1,41 @@
-﻿namespace Soundex
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TDDPractices
 {
     public class Soundex
     {
-        public string AbandonLettersofAEIOUYHW()
+        private readonly List<char> _abandonedLetters=new List<char>()
         {
-            throw new System.NotImplementedException();
+            'a','e','i','o','u','y','h','w'
+        };
+        public string AbandonLettersofAEIOUYHW(string input)
+        {
+            if(string.IsNullOrEmpty(input))
+                throw new ArgumentException("input should not be empty or null");
+            var retainedChars = new List<Char>();
+            retainedChars.Add(input[0]);
+            retainedChars.AddRange(
+                input.Skip(1)
+                .Where(x => !_abandonedLetters.Contains(x)));
+            var retainedOutputStr = new String(retainedChars.ToArray());
+            return retainedOutputStr;
+        }
+
+        private readonly IDictionary<char, int> _cononantNumberMap = new Dictionary<char, int>()
+        {
+            {'b', 1},{'f',1},{'p',1},{'v',1},
+            { 'c',2},{'g',2},{'j',2},{'k',2},{'q',2},{'s',2},{'x',2},{'z',2},
+            { 'd',3},{'t',3},
+            { 'l',4},
+            { 'm',5},{'n',5},
+            { 'r',6}
+        };
+
+        public int ReplaceCononantWithNumber(char c)
+        {
+            return _cononantNumberMap[c];
         }
     }
 }
