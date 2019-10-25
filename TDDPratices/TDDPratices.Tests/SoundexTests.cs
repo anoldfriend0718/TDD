be201aebbbedc74c7d0134a82b97ab78922e7be9
Qianghui Xu <QXu10@slb.com>
2019-10-25 12:37:08 +0800

    create the project

---
+++ TDDPratices/TDDPratices.Tests/SoundexTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDPractices.Tests
+{
+    [TestClass]
+    public class SoundexTests
+    {
+        [TestMethod]
+        public void ShouldRetainFirstLetter()
+        {
+            //Soundex soundex
+        }
+    }
+}
