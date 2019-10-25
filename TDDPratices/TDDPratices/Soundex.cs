7cb8eb1a6bbde2e556a922b0750ba2a24883b2b4
Qianghui Xu <QXu10@slb.com>
2019-10-25 12:43:55 +0800

    refactor Soundex

--- TDDPratices/TDDPratices/Soundex.cs
+++ TDDPratices/TDDPratices/Soundex.cs
@@ -1,8 +1,20 @@
-﻿namespace TDDPractices
+﻿using System;
+
+namespace TDDPractices
 {
     public class Soundex
     {
         public string Encode(string word)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                throw new ArgumentException("Word should not be null or empty");
+            }
+
+            return GetHead(word);
+        }
+
+        private string GetHead(string word)
         {
             return word.Substring(0, 1);
         }
